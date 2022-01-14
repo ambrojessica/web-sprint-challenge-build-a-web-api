e16c3956785496ae27ffb7a3363255b2cffcc92f
ambrojessica <ambrojessica0@gmail.com>
2022-01-14 11:19:07 -0600

    its alivegit add . starting projects router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,18 @@
 // Write your "projects" router here!
+const express = require('express');
+const router = express.Router();
+
+const Projects = require('../projects/projects-model');
+
+//get api projects
+router.get('/', async (req, res) => {
+  try {
+    const project = await Projects.get();
+    res.status(200).json(project);
+  }
+  catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -6,4 +6,28 @@ const server = express();
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
+const projectsRouter = require('./projects/projects-router');
+// const actionsRouter = require('./actions/actions-router');
+
+server.use(express.json());
+
+server.use('/api/projects', projectsRouter);
+// server.use('/api/actions', actionsRouter);
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Sprint Challenge: Build a Web API!</h2>`);
+});
+
+server.use('*', (req, res) => {
+  res.status(404).json({
+    message: `${req.method} ${req.baseUrl} not found`
+  });
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: `${err.message}`
+  });
+});
+
 module.exports = server;
