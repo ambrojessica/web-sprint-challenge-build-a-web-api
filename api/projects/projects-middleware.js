11b1051c51dec30e42ce276ed803719e37b8bebc
ambrojessica <ambrojessica0@gmail.com>
2022-01-14 12:33:05 -0600

    stuck on put, halfway there

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,34 @@
 // add middlewares here related to projects
+
+const Projects = require('./projects-model');
+
+async function validateProjectId(req, res, next) {
+  const projects = await Projects.get(req.params.id);
+  try {
+    if (!projects) {
+      next({
+        status: 404,
+        message: 'project not found'
+      });
+    } else {
+      req.projects = projects;
+      next();
+    }
+  }
+  catch (err) {
+    next(err);
+  }
+}
+
+function validateProject(req, res, next) {
+  if (!req.body.name || !req.body.description || !req.body.completed) {
+    next({
+      status: 400,
+      message: 'required name and description'
+    });
+  } else {
+    next();
+  }
+}
+
+module.exports = { validateProjectId, validateProject };
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,14 +1,39 @@
 // Write your "projects" router here!
 const express = require('express');
+const { restart } = require('nodemon');
 const router = express.Router();
 
 const Projects = require('../projects/projects-model');
+const { validateProjectId, validateProject } = require('./projects-middleware');
 
 //get api projects
 router.get('/', async (req, res) => {
   try {
     const project = await Projects.get();
     res.status(200).json(project);
+  }
+  catch (err) {
+    next(err);
+  }
+});
+
+//get api projects id
+router.get('/:id', validateProjectId, async (req, res) => {
+  const projects = await Projects.get(req.params.id);
+  res.json(projects);
+});
+
+//post api projects
+router.post('/', validateProject, (req, res) => {
+  Projects.insert(req.body);
+  res.json(req.body);
+});
+
+//put api projects id
+router.put('/:id', validateProjectId, async (req, res) => {
+  try {
+    const updatedProject = await Projects.update(req.params.id, req.body);
+    res.status(200).json(updatedProject);
   }
   catch (err) {
     next(err);
